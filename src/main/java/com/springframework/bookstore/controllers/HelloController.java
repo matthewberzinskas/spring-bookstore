27cc9ce898227318b60733cf05e77105e756b4c9
matthewberzinskas <pasomatt@gmail.com>
2022-08-01 13:43:57 -0700

    Added routing to default and books routes.

--- src/main/java/com/springframework/bookstore/controllers/HelloController.java
+++ src/main/java/com/springframework/bookstore/controllers/HelloController.java
@@ -1,14 +1,14 @@
 package com.springframework.bookstore.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String helloMessage(Model model) {
-        return "books/hello";
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
     }
+
 }
